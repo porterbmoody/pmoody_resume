97a591f8268bb23de6a1618d84cbf84dc3482d87
porterbmoody <porterbmoody@gmail.com>
2023-03-17 06:03:47 -0700

    f

---
+++ myproject/myproject/my_swag_bot.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+class SwagBot():
+    def __init__(self) -> None:
+        pass
+
+    def process_message(message):
+
+        if message == "yo!":
+            return "yo wutup!!"
+        
+        return "bruuuuh"
